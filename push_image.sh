65288d2cdeab5353baead82af4a63b1100d8e022
Fred Bitenyo <fredbitenyo@gmail.com>
2023-02-18 12:45:18 +0200

    fixing the docker hub repo

--- push_image.sh
+++ push_image.sh
@@ -2,8 +2,12 @@
 
 set -eu
 
+
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 # use the docker tag command to give the image a new name
-docker tag $IMAGE_TAG $DOCKER_USERNAME/$DOCKER_HUB_REPO_NAME
+docker tag $DOCKER_USERNAME/$IMAGE_TAG:1.0 $DOCKER_USERNAME/$DOCKER_HUB_REPO_NAME
 # push the image to your docker hub repo
 docker push $DOCKER_USERNAME/$DOCKER_HUB_REPO_NAME
+
+# docker tag local-image:tagname new-repo:tagname
+# docker push new-repo:tagname
