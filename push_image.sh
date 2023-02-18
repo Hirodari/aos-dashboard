8706dae5e02700345f891661ebecb45bce2dc8ac
Fred Bitenyo <fredbitenyo@gmail.com>
2023-02-18 12:53:10 +0200

    last fixing push image script

--- push_image.sh
+++ push_image.sh
@@ -5,7 +5,7 @@ set -eu
 
 docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
 # use the docker tag command to give the image a new name
-docker tag $DOCKER_USERNAME/$IMAGE_TAG:latest $DOCKER_USERNAME/$DOCKER_HUB_REPO_NAME
+docker tag $DOCKER_USERNAME/$IMAGE_TAG $DOCKER_USERNAME/$DOCKER_HUB_REPO_NAME
 # push the image to your docker hub repo
 docker push $DOCKER_USERNAME/$DOCKER_HUB_REPO_NAME
 
